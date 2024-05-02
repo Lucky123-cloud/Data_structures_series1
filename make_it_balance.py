3954921bcde07a19b12aa4b298f60ae4f111e59f
Lucky-cloud 123 <luckybaraka@gmail.com>
2024-05-02 15:29:05 +0300

    added make it balance

--- make_it_balance.py
+++ make_it_balance.py
@@ -18,3 +18,44 @@
 
 # Output: False
 
+class Stack:
+    def __init__(self):
+        self.stack_elements = []
+
+    def push(self, item):
+        self.stack_elements.append(item)
+
+    def top(self):
+        if self.stack_elements:
+            return self.stack_elements[-1]
+        else:
+            return None
+
+    def pop(self):
+        if self.stack_elements:
+            return self.stack_elements.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack_elements)
+
+    def empty(self):
+        return len(self.stack_elements) == 0
+
+def isBalanacedParentheses(s):
+    # Write code here
+    stack = Stack()
+
+    # Dictionary to store matching pairs of parentheses
+    matching = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in ['(', '{', '[']:
+            stack.push(char)
+        elif char in [')', '}', ']']:
+            if stack.empty() or stack.top() != matching[char]:
+                return False
+            stack.pop()
+
+    return stack.empty()
